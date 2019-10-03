9863b7890f95702ad8865676454a55b7b88f3a54
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-03 21:32:57 +0200

    Add .bashrc

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ echo -e "\u001b[32;1mSetting up Dotfiles...\u001b[0m"
 echo -e "\u001b[33;1m Backing up existing files... \u001b[0m"
 mv -iv ~/.xsession ~/.xsession.old
 mv -iv ~/.vimrc ~/.vimrc.old
+mv -iv ~/.bashrc ~/.bashrc.old
 mv -iv ~/suckless/dwm/config.h ~/suckless/dwm/config.h.old
 mv -iv ~/suckless/st/config.h ~/suckless/st/config.h.old
 
@@ -18,6 +19,7 @@ mv -iv ~/suckless/st/config.h ~/suckless/st/config.h.old
 echo -e "\u001b[36;1mAdding symlinks...\u001b[0m"
 ln -sfnv $PWD/.xsession ~/.xsession
 ln -sfnv $PWD/.vimrc ~/.vimrc
+ln -sfnv $PWD/.bashrc ~/.bashrc
 ln -sfnv $PWD/suckless/dwm/config.h ~/suckless/dwm/config.h
 ln -sfnv $PWD/suckless/st/config.h ~/suckless/st/config.h
 
