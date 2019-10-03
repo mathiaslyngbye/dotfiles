f397f70aa75fdb3b34f914c4393eb673f0ae1bb6
mathiaslyngbye <malyn16@student.sdu.dk>
2019-10-03 12:20:40 +0200

    Remove .Xresources

--- dot.sh
+++ dot.sh
@@ -9,13 +9,11 @@ echo -e "\u001b[32;1mSetting up Dotfiles...\u001b[0m"
 
 # Back up existing files
 echo -e "\u001b[33;1m Backing up existing files... \u001b[0m"
-mv -iv ~/.Xresources ~/.Xresources.old
 mv -iv ~/.xsession ~/.xsession
 mv -iv ~/.vimrc ~/.vimrc.old
 
 # Create symbolic links
 echo -e "\u001b[36;1mAdding symlinks...\u001b[0m"
-ln -sfnv $PWD/.Xresources ~/.Xresources
 ln -sfnv $PWD/.xsession ~/.xsession
 ln -sfnv $PWD/.vimrc ~/.vimrc
 
